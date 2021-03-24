06a9dc6242303afc48f019e2dfcea61fc182c6d9
Shannon TJ <shannontj@shaw.ca>
2021-03-23 23:56:04 -0600

    drink schema

---
+++ server/models/drinkModel.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const drinkSchema = mongoose.Schema({
+  selectedFile: String,
+  name: String,
+  type: String,
+  brewery: String,
+  score: {
+    type: Number,
+    default: 0,
+  },
+  comments: String,
+  username: String,
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
