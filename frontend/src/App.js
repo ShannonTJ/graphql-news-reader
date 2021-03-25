13d4b4c5db7eb04c4d1506f981839c238a2bc7b5
Shannon TJ <shannontj@shaw.ca>
2021-03-24 23:29:34 -0600

    components setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { FaBeer } from "react-icons/fa";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/Posts";
 
 const App = () => {
   return (
@@ -11,6 +13,23 @@ const App = () => {
         </Typography>
         <FaBeer />
       </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretcg"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
     </Container>
   );
 };
