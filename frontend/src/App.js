97b01f517899fa210cce26439551059a7a001c65
Shannon TJ <shannontj@shaw.ca>
2021-03-24 23:23:39 -0600

    initial styling of app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { FaBeer } from "react-icons/fa";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hi</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          DrinksApp
+        </Typography>
+        <FaBeer />
+      </AppBar>
+    </Container>
   );
 };
 
