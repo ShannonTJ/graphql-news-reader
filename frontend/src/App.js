97dd7bb75964071300557ecf03a796277f7ae85e
Shannon TJ <shannontj@shaw.ca>
2021-03-25 21:42:15 -0600

    add redux to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getPosts } from "./actions/posts";
+
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import { FaBeer } from "react-icons/fa";
 import Form from "./components/Form/Form";
@@ -7,6 +10,12 @@ import useStyles from "./styles";
 
 const App = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+
   return (
     <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
