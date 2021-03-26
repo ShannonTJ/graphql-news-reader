8b92f060d649e953c33fbac31eb483aae8464799
Shannon TJ <shannontj@shaw.ca>
2021-03-25 22:28:49 -0600

    form setup

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { TextField, Button, Typography, Paper } from "@material-ui/core";
 import useStyles from "./styles";
 
 const Form = () => {
+  const [drinkData, setDrinkData] = useState({
+    name: "",
+    type: "",
+    brewery: "",
+    selectedFile: "",
+    comments: "",
+    username: "",
+  });
   const classes = useStyles();
-  return <div>Form</div>;
+
+  const handleSubmit = () => {};
+
+  return (
+    <Paper className={classes.paper}>
+      <form
+        autoComplete="off"
+        noValidate
+        className={classes.form}
+        onSubmit={handleSubmit}
+      >
+        <Typography variant="h6">Creating a Drink</Typography>
+        <TextField
+          name="creator"
+          variant="outlined"
+          label="Creator"
+          fullWidth
+          value={drinkData.username}
+          onChange={(e) => setDrinkData({ username: e.target.value })}
+        />
+      </form>
+    </Paper>
+  );
 };
 
 export default Form;
