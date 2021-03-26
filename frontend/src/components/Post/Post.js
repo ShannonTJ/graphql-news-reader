071810769063da626822e5e0eaf5635adb142ad0
Shannon TJ <shannontj@shaw.ca>
2021-03-26 16:24:04 -0600

    style individual posts

--- frontend/src/components/Post/Post.js
+++ frontend/src/components/Post/Post.js
@@ -1,9 +1,56 @@
 import React from "react";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core";
+import { FaStar } from "react-icons/fa";
+import { FaTrash } from "react-icons/fa";
+import { FaEllipsisH } from "react-icons/fa";
+
+import moment from "moment";
 import useStyles from "./styles";
 
-const Post = () => {
+const Post = ({ post }) => {
   const classes = useStyles();
-  return <div>Post</div>;
+  return (
+    <Card className={classes.card}>
+      <CardMedia
+        className={classes.media}
+        image={post.selectedFile}
+        title={post.name}
+      />
+      <div className={classes.overlay}>
+        <Typography variant="h6">{post.username}</Typography>
+        <Typography variant="body2">
+          {moment(post.createdAt).fromNow()}
+        </Typography>
+      </div>
+      <div className={classes.overlay2}>
+        <Button style={{ color: "white" }} size="small" onClick={() => {}}>
+          <FaEllipsisH fontSize="default" />
+        </Button>
+      </div>
+      <CardContent>
+        <div className={classes.title}>
+          <Typography variant="h5" gutterBottom>
+            {post.comments}
+          </Typography>
+        </div>
+      </CardContent>
+      <CardActions>
+        <Button size="small" color="primary" onClick={() => {}}>
+          <FaStar fontSize="small" /> Like {post.score}
+        </Button>
+        <Button size="small" color="primary" onClick={() => {}}>
+          <FaTrash fontSize="small" /> Delete
+        </Button>
+      </CardActions>
+    </Card>
+  );
 };
 
 export default Post;
