a1f54e4fa417c4a4b6da542ac47e1597683044e5
Shannon TJ <shannontj@shaw.ca>
2021-03-26 18:08:19 -0600

    add deletepost to redux

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -28,7 +28,7 @@ export const updatePost = async (req, res) => {
   const drink = req.body;
 
   if (!mongoose.Types.ObjectId.isValid(_id)) {
-    return res.status(404).send("No post with that id");
+    return res.status(404).send("No drink with that id");
   }
 
   const updatedDrink = await DrinkModel.findByIdAndUpdate(
@@ -40,4 +40,15 @@ export const updatePost = async (req, res) => {
   );
 
   res.json(updatedDrink);
+};
+
+export const deletePost = async (req, res) => {
+  const { id: _id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    return res.status(404).send("No drink with that id");
+  }
+
+  await DrinkModel.findByIdAndRemove(_id);
+  res.json({ message: "Drink deleted successfully" });
 };
