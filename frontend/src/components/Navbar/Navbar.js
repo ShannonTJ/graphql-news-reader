254e63e2b416671079b9661c2d2aa7143e1fbf77
Shannon TJ <shannontj@shaw.ca>
2021-03-27 20:14:27 -0600

    create navbar component

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,17 +1,60 @@
 import React from "react";
-import { AppBar, Typography } from "@material-ui/core";
+import { AppBar, Typography, Toolbar, Button } from "@material-ui/core";
+import { Link } from "react-router-dom";
 import { FaBeer } from "react-icons/fa";
 
 import useStyles from "./styles";
 
 const Navbar = () => {
   const classes = useStyles();
+  const user = null;
+
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
-      <FaBeer className={classes.image} height="60" />
-      <Typography className={classes.heading} variant="h2" align="center">
-        DrinksApp
-      </Typography>
+      <div className={classes.brandContainer}>
+        <FaBeer className={classes.image} height="60" />
+        <Typography
+          component={Link}
+          to="/"
+          className={classes.heading}
+          variant="h2"
+          align="center"
+        >
+          DrinksApp
+        </Typography>
+      </div>
+      <Toolbar className={classes.toolbar}>
+        {user ? (
+          <div className={classes.profile}>
+            <Avatar
+              className={classes.purple}
+              alt={user.result.name}
+              src={user.result.imageUrl}
+            >
+              {user.result.name.charAt(0)}
+            </Avatar>
+            <Typography className={classes.userName} variant="h6">
+              {user.result.name}
+            </Typography>
+            <Button
+              variant="contained"
+              className={classes.logout}
+              color="secondary"
+            >
+              Log out
+            </Button>
+          </div>
+        ) : (
+          <Button
+            component={Link}
+            to="/auth"
+            variant="contained"
+            color="primary"
+          >
+            Log in
+          </Button>
+        )}
+      </Toolbar>
     </AppBar>
   );
 };
